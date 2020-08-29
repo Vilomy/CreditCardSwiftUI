6d7a710dee215f88babf714052ed009927be2a25
Gleb Zadonskiy <vilomy2@gmail.com>
2020-08-29 23:37:00 +0300

    Initial Commit

---
+++ Shared/CreditCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardSwiftUIApp.swift
+//  Shared
+//
+//  Created by Gleb Zadonskiy on 29.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
